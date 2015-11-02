715bc7be98fe9a2d3cb7998a37c75d09e1d491fe
kalle (jag) <kalle.jillheden@me.com>
2015-11-02 14:16:09 +0100

    End of tutor 1

---
+++ Assets/Scripts/MoverScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverScript : MonoBehaviour {
+
+    public Rigidbody2D rbody;
+
+    [Space]
+
+    public Vector2 speed = - Vector2.left * 10f;
+	
+    void Update() {
+
+        rbody.velocity = speed;
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody2D rbody;
+
+    [Space]
+
+    public Vector2 speed = Vector2.one * 50f;
+
+    void Update() {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(xAxis * speed.x, yAxis * speed.y);
+        //movement *= Time.deltaTime;
+
+        rbody.velocity = movement;
+    }
+
+}
