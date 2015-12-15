3a3d5cabacf69e47dbd27fd9c1811a1625020e46
kalle (jag) <kalle.jillheden@me.com>
2015-12-15 10:38:25 +0100

    Parallax

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScript : MonoBehaviour {
+
+	[Range(0f,1f)]
+	public float scale = 1f;
+	public float offset = 0f;
+	public float offsetMotion = 0f;
+	public bool wrap = false;
+	public float wrapAfter = 64f;
+
+	void LateUpdate () {
+		var pos = transform.position;
+
+		// Alter
+		offset += offsetMotion * Time.deltaTime;
+
+		pos.x = Camera.main.transform.position.x * scale + offset;
+
+		if (wrap)
+			offset %= wrapAfter;
+
+		// Apply
+		transform.position = pos;
+	}
+}
